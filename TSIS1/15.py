e3a7dc75e48f9708e6121f39a53bf121c78786bd
Арайлым Кабыкенова <arajlymkabykenova@MacBook-Air-Arajlym.local>
2024-01-22 01:04:07 +0600

    a

---
+++ TSIS1/15.py
@@ -0,0 +1,18 @@
+a="Hello World"
+print("World" in a)
+
+b="Good morning"
+print("goodbye" not in b)
+print("morning" not in b)
+
+x="Because I am happy!"
+if "I" in x:
+    print("Yes")
+else:
+    print("No")
+
+x="Because I am happy!"
+if "I" not in x:
+    print("Yes")
+else:
+    print("No")
