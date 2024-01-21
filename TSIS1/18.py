e892bbdd711b58cca622756ba7769c8ab1a8a102
Арайлым Кабыкенова <arajlymkabykenova@MacBook-Air-Arajlym.local>
2024-01-22 02:05:46 +0600

    a

---
+++ TSIS1/18.py
@@ -0,0 +1,9 @@
+drink=0.5
+num=2
+act=3
+txt="I drink {} l {} bottle of water and after i slept {} hours "
+print(txt.format(drink,num,act))
+txt_1="I drink {0} l {1} bottle of water and after i slept {2} hours "
+print(txt_1.format(drink,num,act))
+txt_2="Hello, \"KBTU\"!"
+print(txt_2)
