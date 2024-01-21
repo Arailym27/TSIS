cee5a97ff1051050fbc334ac8668b2d625ab13d7
Арайлым Кабыкенова <arajlymkabykenova@MacBook-Air-Arajlym.local>
2024-01-22 02:05:22 +0600

    a

---
+++ TSIS1/16.py
@@ -0,0 +1,21 @@
+b="Good luck"
+print (b[5:9])
+#print (b[0:4])
+print (b[:4])
+print (b[2:])
+
+#print (b[:])а так можно?
+
+print(b[-9:])
+print(b[-9:-4])
+
+print(b.upper())
+print(b.lower())
+
+a=" Hello  "
+print(a.strip())
+print(a.lstrip())#left
+print(a.rstrip())#right
+print(a.replace('He','A'))
+print(b.split(" "))
+print(b.split("d"))
