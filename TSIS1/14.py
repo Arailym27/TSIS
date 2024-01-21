f88f66e77e81b7433de685b0a2a76eef7651021a
Арайлым Кабыкенова <arajlymkabykenova@MacBook-Air-Arajlym.local>
2024-01-22 01:03:47 +0600

    a

---
+++ TSIS1/14.py
@@ -0,0 +1,23 @@
+a="Hello"
+for i in a:
+    print(i)
+print (a)
+print(a[0])
+print(len(a))
+
+
+x="""   Uaimga salinba,jakynyn janynda
+jurek jolyn tandada,asykpai kadamda
+bir manici bolady,bakyt kusy konady
+        !bari jaksy bolady!"""
+print(x)
+print(len(x))
+
+b='''Wasdfghjkl
+sdfghjkl
+dfghjkl
+sdfghjk cjkl'''
+print(b)
+
+
+#в предложениях как сами ставим так и выводится
