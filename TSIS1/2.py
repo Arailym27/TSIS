136d0dfe59852895295f70728bc19028536961de
Арайлым Кабыкенова <arajlymkabykenova@MacBook-Air-Arajlym.local>
2024-01-21 23:18:35 +0600

    smth

---
+++ TSIS1/2.py
@@ -0,0 +1,19 @@
+x=6
+a='a'
+s="Hello!"
+if 5>2:
+    print("Five is greater than 2!")
+if 5>2:
+ print("Five is greater than 2!")
+print(x,a,s)
+print(x)
+print(a)
+print(s)
+
+#if 5>2:
+#print("We will get an error,cause we do not paste tab")
+#oтступ должен быть как минимум 1(а лучше 4)
+"""if 5>2:
+    print("Five is greater than 2!")
+ print("Five is greater than 2!")"""
+ #intentation error
