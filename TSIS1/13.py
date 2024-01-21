1bf623e37d2fbc628ae9e429fd9272c6117a0d24
Арайлым Кабыкенова <arajlymkabykenova@MacBook-Air-Arajlym.local>
2024-01-22 00:38:31 +0600

    transform types

---
+++ TSIS1/13.py
@@ -0,0 +1,12 @@
+x = int(1)   # x will be 1
+y=int(1.1)
+z=int('1')
+
+a = float(1)    # x will be 1.0
+b=float(1.2)
+c=float("1")
+d=float("1,2")
+
+s = str("s1") # x will be 's1'
+e=str(9)
+g=str(3.2)
