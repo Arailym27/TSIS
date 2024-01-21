e37172c09cf0925f05f48eb85536c6d9e4a15c2e
Арайлым Кабыкенова <arajlymkabykenova@MacBook-Air-Arajlym.local>
2024-01-22 02:05:58 +0600

    a

---
+++ TSIS1/19.py
@@ -0,0 +1,31 @@
+txt = 'It\'s alright.'
+print(txt) 
+
+txt_2="Hello, \"KBTU\"!"
+print(txt_2)
+
+txt_3 = "This will insert one \\ (backslash)."
+print(txt_3) 
+#This will insert one \ (backslash).
+
+txt_4 = "4Hello\nWorld!"
+print(txt_4) 
+
+txt5 = "5 Hello\rWorld!"#сверху идет и заменяет
+print(txt5) 
+
+txt6 = "Hello\tWorld!"
+print(txt6) 
+
+txt7 = "Hello \bWorld!"
+print(txt7) 
+
+a = "\110\145\154\154\157"
+print(a) 
+
+b = "\x48\x65\x6c\x6c\x6f"
+print(b) 
+
+
+
+
