26fcd1049fcb352ae3f78a8fe7bae967d8b57cc2
Арайлым Кабыкенова <arajlymkabykenova@MacBook-Air-Arajlym.local>
2024-01-21 23:23:28 +0600

    Output Variables

---
+++ TSIS1/6.py
@@ -0,0 +1,14 @@
+x=0
+y="zero"
+z=5
+a='is'
+b='number'
+r="zero "
+f='is '
+n='number'
+print(x, y)
+print(x+z)
+print(y +a +b )#пробел не играет роль
+print(y, a, b)#пробел ставит пробел
+print(r+f+n)#при сумме изначально элементы должны иметь пробелы 
+#but not print(x+y),we can not produce the sum of two different types
