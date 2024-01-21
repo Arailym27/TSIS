735c2a2c51d5940c1ea4ab0bd25f05e1cfda5f3e
Арайлым Кабыкенова <arajlymkabykenova@MacBook-Air-Arajlym.local>
2024-01-21 23:22:32 +0600

    Assign Multiple Values

---
+++ TSIS1/5.py
@@ -0,0 +1,13 @@
+fruits=["apple","banana","cherry"]
+d,e,f=fruits
+print(d)
+print(e)
+print(f)
+x,y,z='Strawberry','Banana','Orange'
+a=b=c="Blueberry"
+print(x)
+print(y)
+print(z)
+print(a)
+print(b)
+print(c)
