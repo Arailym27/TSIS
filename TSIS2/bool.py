1e4b77636de64cd5644c407944cd40c800ef2f52
Арайлым Кабыкенова <arajlymkabykenova@MacBook-Air-Arajlym.local>
2024-01-28 22:18:06 +0600

    bool

--- TSIS2/bool.py
+++ TSIS2/bool.py
@@ -1,1 +1,28 @@
-print("Hello World")
+print(10>9)
+print(10 == 9)
+print(10 < 9)
+
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+x = "Hello"
+y = 15
+print(bool(x))
+print(bool(y))
+print(bool())
+print(bool("abc"))
+print(bool(123))
+print(bool(["apple", "cherry", "banana"]))
+
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
