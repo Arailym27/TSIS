d3baee04f55c3a079122326cbbde13f20c9fbbab
Арайлым Кабыкенова <arajlymkabykenova@MacBook-Air-Arajlym.local>
2024-02-06 10:57:17 +0600

    upstr

--- TSIS3/Classes/a.py
+++ TSIS3/Classes/a.py
@@ -1,1 +1,9 @@
-print("Hello , World")
+class string_up:
+    def get_string(self):
+        self.s=input()
+
+    def print_string(self):
+        print(self.s.upper())
+answer=string_up()
+answer.get_string()
+answer.print_string()
