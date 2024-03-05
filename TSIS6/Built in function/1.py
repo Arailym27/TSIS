8f6dc640ea91f9fb9cce21b7e32a96b2c7b18897
Арайлым Кабыкенова <arajlymkabykenova@MacBook-Air-Arajlym.local>
2024-03-05 23:28:13 +0500

    new

---
+++ TSIS6/Built in function/1.py
@@ -0,0 +1,6 @@
+# Write a Python program with builtin function to multiply all the numbers in a list
+import math 
+num = [1 , 2 , 3, 4 , 5]
+result = math.prod(num)
+# math.prod() для умножения всех чисел в этом списке. 
+print(result)
